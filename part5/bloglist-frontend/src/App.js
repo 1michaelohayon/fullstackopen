000883fd23bf51565ed101775aa629a114d26f6d
1michaelohayon <1michaelohayon@gmail.com>
2022-09-02 16:25:59 +0300

    rui

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,30 @@
-import LoginUI from './components/LoginUI'
+import { useState } from "react"
+import LoginForm from "./components/LoginForm"
+import BlogList from "./components/BlogList"
+import BlogForm from "./components/BlogForm"
+
 const App = () => {
+
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+
+  const logout = () => {
+    window.localStorage.removeItem('loggedBlogListUser')
+    setUser(null)
+  }
 
   return (
     <>
-      <LoginUI/>
+      {user === null
+        ? <LoginForm setUser={setUser} />
+        : <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged in<button onClick={logout}>logout</button></p>
+          <h2>create new</h2>
+          <BlogForm blogs={blogs} setBlogs={setBlogs} />
+          <BlogList blogs={blogs} setBlogs={setBlogs} />
+        </div>
+      }
     </>
   )
 
